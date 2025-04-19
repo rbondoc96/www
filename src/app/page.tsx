cefde226d9a27214c2f9fe26687c868ed68388f2
Rodrigo Bondoc <rbondoc96@gmail.com>
2025-04-19 16:15:48 -0700

    fix: missing theme switch on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 import { type ReactNode } from 'react';
 import { SiteNavigation } from '@/app/navigation';
+import { ThemeSwitch } from '@/app/theme-provider';
 import { orbitron } from '@/app/ui/fonts';
 
 export default function Page(): ReactNode {
     return (
-        <main className="flex-1 flex flex-col">
-            <div className="flex-1 flex flex-col justify-center items-center gap-6 mb-12">
-                <h1 className={`${orbitron.className} font-semibold text-8xl md:text-9xl tracking-tight`}>RDB</h1>
-                <p className="font-light text-center text-sm md:text-lg">Rodrigo Bondoc · San Francisco, CA</p>
-                <SiteNavigation />
-            </div>
-        </main>
+        <>
+            <header>
+                <div className="flex justify-end my-8">
+                    <ThemeSwitch />
+                </div>
+            </header>
+            <main className="flex-1 flex flex-col">
+                <div className="flex-1 flex flex-col justify-center items-center gap-6 mb-12">
+                    <h1 className={`${orbitron.className} font-semibold text-8xl md:text-9xl tracking-tight`}>RDB</h1>
+                    <p className="font-light text-center text-sm md:text-lg">Rodrigo Bondoc · San Francisco, CA</p>
+                    <SiteNavigation />
+                </div>
+            </main>
+        </>
     );
 }
